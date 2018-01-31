4e6be6c215d8509689cab2100ccf071a7dca48ae
Brain Tucker <brian.briantucker@gmail.com>
2018-01-31 12:20:39 -0600

    each spot has a tag and that tag is logged in the console when clicked, also fixed layout

--- app/src/main/java/com/briantucker/sportsconnect/MainActivity.java
+++ app/src/main/java/com/briantucker/sportsconnect/MainActivity.java
@@ -7,14 +7,35 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //0 = baseball, 1 = basketball, 2 = soccerball
+
+
+    int activePlayer = 0;
+
+    //3 means unplayed
+
+    int[] gameState = {3, 3, 3, 3, 3, 3, 3, 3, 3};
+
 
     public void dropIn(View view) {
 
+        //possibly allow an option to steal a spot? Currently the balls can replace each other if they are clicked
+
+
         ImageView counter = (ImageView) view;
+
+        System.out.println(counter.getTag().toString());
 
         counter.setTranslationY(-1000f);
 
-        counter.setImageResource(R.drawable.baseball);
+        if (activePlayer == 0) {
+            counter.setImageResource(R.drawable.baseball);
+            activePlayer = 1;
+        }
+        else {
+            counter.setImageResource(R.drawable.basketball);
+            activePlayer = 0;
+        }
 
         counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
 
