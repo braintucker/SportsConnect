9268fb56fdef82e649a6bdc90b3ff1ae04dd92c9
Brain Tucker <brian.briantucker@gmail.com>
2018-01-31 13:17:09 -0600

    when a player has won, a play again prompt appears, button doesn't work yet

--- app/src/main/java/com/briantucker/sportsconnect/MainActivity.java
+++ app/src/main/java/com/briantucker/sportsconnect/MainActivity.java
@@ -2,20 +2,26 @@ package com.briantucker.sportsconnect;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    //0 = baseball, 1 = basketball, 2 = soccerball
+    //0 = baseball, 1 = basketball
 
 
     int activePlayer = 0;
 
     //3 means unplayed
 
-    int[] gameState = {3, 3, 3, 3, 3, 3, 3, 3, 3};
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
 
+    int[][] winningPositions ={{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
 
     public void dropIn(View view) {
 
@@ -23,16 +29,13 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView counter = (ImageView) view;
 
-        System.out.println(counter.getTag().toString());
 
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
-        if (gameState[tappedCounter] == 3 ) {
+        if (gameState[tappedCounter] == 2 ) {
 
             gameState[tappedCounter] = activePlayer;
-
             counter.setTranslationY(-1000f);
-
             if (activePlayer == 0) {
                 counter.setImageResource(R.drawable.baseball);
                 activePlayer = 1;
@@ -43,10 +46,39 @@ public class MainActivity extends AppCompatActivity {
 
             counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
 
-        }
+            for(int[] winningPosition : winningPositions){
 
+                if (gameState[winningPosition[0]] == gameState[winningPosition[1]] &&
+                        gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2){
+
+                    //Someone has won
+
+                    String winner = "Basketball";
+                    if (gameState[winningPosition[0]] == 0){
+                        winner = "Baseball";
+                    }
+
+
+
+                    TextView winnerMessage = (TextView) findViewById(R.id.winnerMessage);
+
+                    winnerMessage.setText(winner + " has won!");
+
+                    LinearLayout layout = (LinearLayout)findViewById(R.id.playAgainLay);
+                    layout.setVisibility(View.VISIBLE);
+                }
+            }
+        }
     }
 
+    public void playAgain() {
+        LinearLayout layout = (LinearLayout)findViewById(R.id.playAgainLay);
+
+        layout.setVisibility(View.INVISIBLE);
+
+        
+
+    }
 
 
     @Override
