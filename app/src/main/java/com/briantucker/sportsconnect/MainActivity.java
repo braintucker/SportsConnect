349eeceecea1eb2697c684878e0e50d43d0f9c3b
Brain Tucker <brian.briantucker@gmail.com>
2018-01-31 13:24:07 -0600

    play again button works and resets game

--- app/src/main/java/com/briantucker/sportsconnect/MainActivity.java
+++ app/src/main/java/com/briantucker/sportsconnect/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.view.View;
+import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -71,13 +72,22 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void playAgain() {
+    public void playAgain(View view) {
         LinearLayout layout = (LinearLayout)findViewById(R.id.playAgainLay);
 
         layout.setVisibility(View.INVISIBLE);
 
-        
+        activePlayer = 0;
 
+        for (int i = 0; i<gameState.length; i++) {
+            gameState[i] = 2;
+        }
+
+        GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLay);
+
+        for (int i = 0; i < gridLayout.getChildCount(); i++){
+            ((ImageView) gridLayout.getChildAt(i)).setImageResource(0);
+        }
     }
 
 
